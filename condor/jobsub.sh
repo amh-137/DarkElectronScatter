ca5357b292da706677c480a983ddb62d7cd30f83
Alexander Hergenhan <ahergenhan@vm119.hep.manchester.ac.uk>
2024-03-22 12:04:00 +0000

    added request 1 GB memory

--- condor/jobsub.sh
+++ condor/jobsub.sh
@@ -21,12 +21,13 @@ for index in "${!jobs[@]}"; do
     # Get the argument corresponding to the current index
     job="${jobs[i]}"    # Generate submit file for each job
     submit_file="${script}.sub"
-    echo "executable  = node_setup.sh" > "$submit_file"
-    echo "arguments   = $job" >> "$submit_file"
-    echo "universe    = vanilla" >> "$submit_file"
-    echo "output      = condor_out/${script}.out" >> "$submit_file"
-    echo "error       = condor_out/${script}.err" >> "$submit_file"
-    echo "log         = condor_out/${script}.log" >> "$submit_file"
+    echo "executable     = node_setup.sh" > "$submit_file"
+    echo "arguments      = $job" >> "$submit_file"
+    echo "universe       = vanilla" >> "$submit_file"
+    echo "output         = condor_out/${script}.out" >> "$submit_file"
+    echo "error          = condor_out/${script}.err" >> "$submit_file"
+    echo "log            = condor_out/${script}.log" >> "$submit_file"
+    echo "request_memory = 1 GB" >> "$submit_file"
     echo "queue" >> "$submit_file"    # Submit the job
     condor_submit "$submit_file"
     echo "Job submitted with dat file: $job"
