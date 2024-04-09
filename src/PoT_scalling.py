c17441f139f65f1eabcf93e370ed482c1de51d59
Alex <alex.hergenhan@gmail.com>
2024-04-09 13:30:05 +0100

    docstring

--- src/PoT_scalling.py
+++ src/PoT_scalling.py
@@ -1,3 +1,15 @@
 
 def pot(POT_g4numi : float, ntrials : float, Nm : float, BR : float, omega_int : list) -> float:
+    """
+    Calculate the POT
+    Args:
+        POT_g4numi (float): pot of g4numi
+        ntrials (float): number of trials
+        Nm (float): i dont know
+        BR (float): branching ratio
+        omega_int (list): ??
+
+    Returns:
+        float: POT
+    """
     return (POT_g4numi * ntrials) / (Nm * BR * max(omega_int))
