faa7c09dd7de3f149df57c71fcb542cdfaf7c117
Alex <alex.hergenhan@gmail.com>
2024-04-09 15:07:48 +0100

    added pot scaling

--- src/PoT_scalling.py
+++ src/PoT_scalling.py
@@ -1,5 +1,5 @@
 
-def pot(POT_g4numi : float, ntrials : float, Nm : float, BR : float, omega_int : list) -> float:
+def calculate_pot(POT_g4numi : float, ntrials : float, Nm : float, BR : float, omega_int : list) -> float:
     """
     Calculate the POT
     Args:
@@ -13,3 +13,17 @@ def pot(POT_g4numi : float, ntrials : float, Nm : float, BR : float, omega_int :
         float: POT
     """
     return (POT_g4numi * ntrials) / (Nm * BR * max(omega_int))
+
+
+def pot_scale(number_of_events : float, PoT : float, pot_g4numi : float) -> float:
+    """PoT scale the number of electrons scattered
+
+    Args:
+        number_of_events (float): number of electron events
+        PoT (float): PoT to generate this many electrons
+        pot_g4numi (float): PoT of the numi run 1
+
+    Returns:
+        float: pot scaled number of events
+    """
+    return number_of_events * (pot_g4numi / PoT)
