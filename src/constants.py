3260b1f9271c370773eeae73e700e8ee49f17b4c
Alex <alex.hergenhan@gmail.com>
2024-05-03 15:36:34 +0100

    added constants for plotting, POT ratio

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+# POT
+POT_RUN1_MICROBOONE = 2.2e20
+POT_RUN3_MICROBOONE = 5.2e20
+# cite Luis Dark Tridents
+
+# for plotting
+dt_col_dict = {"0.33":"tab:green", "0.6":"tab:blue", "2.0":"tab:red"}
+dt_marker_dict = {"0.33":"o", "0.6":"x", "2.0":"^"}
